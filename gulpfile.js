888878d88cf5c80817ee9241ebb05e7f062e1d82
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-01-14 21:34:21 -0800

    1.2.3 Configured gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,26 @@ var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     autoprefixer = require('gulp-autoprefixer'),
     del  = require('del'),
-    rename = require('gulp-rename'); 
+    rename = require('gulp-rename');
 
 // NAMED TASKS
 // Scripts Task - tasks related to js
 gulp.task('scripts', function(){
-  gulp.src(['dev/js/**/*.js', '!dev/js/**/*.min.js'])
+  gulp.src(['app/js/**/*.js', '!app/js/**/*.min.js'])
   .pipe(plumber())
   .pipe(rename({suffix:'.min'}))
   .pipe(uglify())
-  .pipe(gulp.dest('dev/js'))
+  .pipe(gulp.dest('app/js'))
   .pipe(reload({stream: true}));
 });
 
 // Sass Tasks - tasks related to sc scss and css
 gulp.task('sass', function(){
-  gulp.src('dev/sass/**/*.scss')
+  gulp.src('app/sass/**/*.scss')
   .pipe(plumber())
+  .pipe(sass({outputStyle: 'expanded'}))
   .pipe(autoprefixer('last 2 versions'))
-  .pipe(gulp.dest('dev/css/'))
+  .pipe(gulp.dest('app/css/'))
   .pipe(reload({stream: true}));
 });
 
@@ -39,16 +40,16 @@ gulp.task('html', function(){
 gulp.task('browser-sync', function(){
   browserSync({
     server:{
-      baseDir: './app/'
+      baseDir: './'
     }
   });
 });
 
 // Watch Task - watch files and folders for changes
 gulp.task('watch', function(){
-  gulp.watch('dev/js/**/*.js', ['scripts']);
-  gulp.watch('dev/sass/**/*.scss', ['sass']);
+  gulp.watch('app/js/**/*.js', ['scripts']);
+  gulp.watch('app/sass/**/*.scss', ['sass']);
 });
 
 // Default Task - runs specified tasks asynchronously
-gulp.task('default', ['scripts', 'compass', 'html', 'php', 'browser-sync', 'watch']);
+gulp.task('default', ['scripts', 'sass', 'html', 'browser-sync', 'watch']);
