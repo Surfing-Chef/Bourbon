ed9e76ddd8f3bc95775feecea3c03ecdeb6ff57e
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-01-23 06:16:16 -0800

    Added appropriate markup, cleaned out custom scripts

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,1 @@
-$('#scene').parallax({
-  calibrateX: false,
-  calibrateY: true,
-  invertX: false,
-  invertY: true,
-  limitX: false,
-  limitY: 10,
-  scalarX: 2,
-  scalarY: 8,
-  frictionX: 0.2,
-  frictionY: 0.8,
-  originX: 0.0,
-  originY: 1.0
-});
 
-$('#scene').parallax();
