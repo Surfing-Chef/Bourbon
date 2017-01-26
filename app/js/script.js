78be52826cd6080466dc5ae40ab314094bd6cd97
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-01-25 16:54:41 -0800

    Loaded up the quotes library

--- app/js/script.js
+++ app/js/script.js
@@ -25,12 +25,12 @@ $(document).ready(function() {
 // Define a quote library
   var quoteSource=[
   {
-    quote: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.",
-    name:"Francis of Assisi"
+    quote: "If you want to become a great chef, you have to work with great chefs. And that's exactly what I did.",
+    name:"Gordon Ramsay"
     },
     {
-      quote:"Believe you can and you're halfway there.",
-      name:"Theodore Roosevelt"
+      quote:"You can call me the bad boy chef all you want. I'm not going to freak out about it. I'm not that bad. I'm certainly not a boy, and it's been a while since I've been a chef.",
+      name:"Anthony Bourdain"
     },
     {
       quote:"It does not matter how slowly you go as long as you do not stop.",
@@ -41,84 +41,84 @@ $(document).ready(function() {
       name:"Thomas A. Edison"
     },
     {
-      quote:"The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence.",
-      name:"Confucius"
+      quote:"If you're not the one cooking, stay out of the way and compliment the chef.",
+      name:"Michael Strahan"
     },
     {
-      quote:"Don't watch the clock; do what it does. Keep going.",
-      name:"Sam Levenson"
+      quote:"A home cook who relies too much on a recipe is sort of like a pilot who reads the plane’s instruction manual while flying.",
+      name:"Alton Brown"
+    },
+    {
+      quote:"First, knife skills. Then, knowing how to control heat. Most important is choosing the right product... the rest is simple.",
+      name:"Justin Quek"
     },
     {
       quote:"A creative man is motivated by the desire to achieve, not by the desire to beat others.",
       name:"Ayn Rand"
     },
     {
-      quote:"A creative man is motivated by the desire to achieve, not by the desire to beat others.",
-      name:"Ayn Rand"
+      quote:"When baking, follow directions. When cooking, go by your own taste",
+      name:"Laiko Bahrs"
     },
     {
-      quote:"Expect problems and eat them for breakfast.",
-      name:"Alfred A. Montapert"
+      quote:"Fish, to taste right, must swim three times – in water, in butter and in wine.",
+      name:"Polish proverb"
     },
     {
-      quote:"Start where you are. Use what you have. Do what you can.",
-      name:"Arthur Ashe"
+      quote:"We're hoping to succeed; we're okay with failure. We just don't want to land in between.",
+      name:"David Chang"
     },
     {
-      quote:"Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.",
-      name:"Samuel Beckett"
-    },
-    {
-      quote:"Be yourself; everyone else is already taken.",
-      name:"Oscar Wilde"
+      quote:"A great restaurant doesn't distinguish itself by how few mistakes it makes but by how well they handle those mistakes.",
+      name:"Danny Meyer"
     },
     {
       quote:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
       name:"Albert Einstein"
     },
     {
-      quote:"Always remember that you are absolutely unique. Just like everyone else.",
-      name:"Margaret Mead"
+      quote:"When you have made as many mistakes as I have then you can be as good as me.",
+      name:"Wolfgang Puck"
     },
     {
-      quote:"Do not take life too seriously. You will never get out of it alive.",
-      name:"Elbert Hubbard"
+      quote:"I would much rather be a chef who remembers I am a cook then a cook that thinks I am a chef.",
+      name:"Richard (Ric) Peterson"
     },
     {
-      quote:"People who think they know everything are a great annoyance to those of us who do.",
-      name:"Isaac Asimov"
+      quote:"I love the man that can smile in trouble, that can gather strength from distress, and grow brave by reflection. ’Tis the business of little minds to shrink, but he whose heart is firm, and whose conscience approves his conduct, will pursue his principles unto death.",
+      name:"Thomas Paine"
     },
     {
-      quote:"Procrastination is the art of keeping up with yesterday.",
-      name:"Don Marquis"
+      quote:"Successful people do what unsuccessful people are not willing to do. Don’t wish it were easier, wish you were better.",
+      name:"Jim Rohn"
     },
     {
-      quote:"Get your facts first, then you can distort them as you please.",
+      quote:"Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails. Explore, Dream, Discover.",
       name:"Mark Twain"
     },
     {
-      quote:"A day without sunshine is like, you know, night.",
-      name:"Steve Martin"
+      quote:"There are two types of people who will tell you that you cannot make a difference in this world: those who are afraid to try and those who are afraid you will succeed.",
+      name:"Ray Goforth"
     },
     {
-      quote:"My grandmother started walking five miles a day when she was sixty. She's ninety-seven now, and we don't know where the hell she is.",
-      name:"Ellen DeGeneres"
+      quote:"There will be obstacles. There will be doubters. There will be mistakes. But with hard work, there are no limits.",
+      name:"Michael Phelps"
     },
     {
-      quote:"Don't sweat the petty things and don't pet the sweaty things.",
-      name:"George Carlin"
+      quote:"When I was 5 years old, my mother always told me that happiness was the key to life. When I went to school, they asked me what I wanted to be when I grew up. I wrote down “happy”. They told me I didn’t understand the assignment, and I told them they didn’t understand life.",
+      name:"John Lennon"
     },
     {
-      quote:"Always do whatever's next.",
-      name:"George Carlin"
+      quote:"Vegetarians, and their Hezbollah-like splinter faction, the vegans ... are the enemy of everything good and decent in the human spirit.",
+      name:"Anthony Bourdain"
     },
     {
-      quote:"Atheism is a non-prophet organization.",
-      name:"George Carlin"
+      quote:"Oh, I'll accomodate them, I'll rummage around for something to feed them, for a 'vegetarian plate', if called on to do so. Fourteen dollars for a few slices of grilled eggplant and zucchini suits my food cost fine.",
+      name:"Anthony Bourdain"
     },
     {
-      quote:"Hapiness is not something ready made. It comes from your own actions.",
-      name:"Dalai Lama"
+      quote:"Few things are more beautiful to me than a bunch of thuggish, heavily tattooed line cooks moving around each other like ballerinas on a busy Saturday night. Seeing two guys who'd just as soon cut each other's throats in their off hours moving in unison with grace and ease can be as uplifting as any chemical stimulant or organized religion.",
+      name:"Anthony Bourdain"
     }
 
   ];
