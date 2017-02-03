9885e6a3bc379cc4b74d3a03dfcdeb6f12c15cf0
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-02-02 18:52:13 -0800

    simplified gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,24 @@
 // REQUIRED
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    reload = browserSync.reload,
-    plumber = require('gulp-plumber'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    concat  = require('gulp-concat'),
-    del  = require('del'),
-    rename = require('gulp-rename');
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
 
-// DEVELOPMENT TASKS
-// Scripts Task - tasks related to js
-gulp.task('scripts', function(){
-  gulp.src(['app/js/**/*.js', '!app/js/**/*min.js'])
-  .pipe(plumber())
-  .pipe(concat('script.min.js'))
-  .pipe(gulp.dest('app/js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('app/js'))
-  .pipe(reload({stream: true}));
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
+  .pipe(sass({
+    'outputStyle' : 'compressed'
+  }))
+  .pipe(gulp.dest('./'))
+  .pipe(browserSync.stream());
 });
 
-// Sass Tasks - tasks related to sc scss and css
-// deployment css - compressed
-gulp.task('sassDep', function(){
-  gulp.src('app/sass/**/*.scss')
-  .pipe(plumber())
-  .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(autoprefixer('last 2 versions'))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('app/css/'))
-  .pipe(reload({stream: true}));
-});
-
-// development css - nested
-gulp.task('sassDev', function(){
-  gulp.src('app/sass/**/*.scss')
-  .pipe(plumber())
-  .pipe(rename({suffix:'.dev'}))
-  .pipe(sourcemaps.init())
-    .pipe(sass({sourceComments: 'map', sourceMap: 'sass', outputStyle: 'nested'}))
-    .pipe(autoprefixer('last 2 versions'))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('app/css/'));
-});
-
-// HTML Tasks - tasks related to html
-gulp.task('html', function(){
-  gulp.src('app/')
-  .pipe(reload({stream: true}));
-});
-
-// Browser-Sync Tasks - tasks related to browser-sync
-gulp.task('browser-sync', function(){
-  browserSync({
-    server:{
-      baseDir: './app/'
-    }
+gulp.task('serve', ['sass'], function(){
+  browserSync.init({
+    proxy   : "http://localhost/Bourbon/app"
   });
+
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./**/*.php').on('change', browserSync.reload);
 });
 
-// DEPLOYMENT TASKS
-// clear out all files and folders from build folder
-gulp.task('build:cleanfolder', function(){
-  del([
-    './build/**/*'
-  ]);
-});
-
-// create build directory for all files
-gulp.task('build:copy', ['build:cleanfolder'], function(){
-  return gulp.src('./app/**/*/')
-  .pipe(gulp.dest('./build/'));
-});
-
-// remove unwanted build files and directories
-gulp.task('build:remove', ['build:copy'], function(done){
-  del([  // list files and directories to delete
-    './build/sass/',
-    './build/css/*dev*'
-  ], done);
-});
-
-// main build task
-gulp.task('build', ['build:copy', 'build:remove']);
-
-// Watch Task - watch files and folders for changes
-gulp.task('watch', function(){
-  gulp.watch('app/js/**/*.js', ['scripts']);
-  gulp.watch('app/sass/**/*.scss', ['sassDev']);
-  gulp.watch('app/sass/**/*.scss', ['sassDep']);
-  gulp.watch('app/**/*.html', ['html']);
-});
-
-// Default Task - runs specified tasks asynchronously
-gulp.task('default', ['scripts', 'sassDev', 'sassDep', 'html', 'browser-sync', 'watch']);
+gulp.task('default', ['serve']);
