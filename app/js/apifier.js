5e8f033daeee64de5bad1d20c2871cebc72ab81d
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-02-10 15:40:34 -0800

    Script to load Apifier scraped culinary  feeds

--- app/js/apifier.js
+++ app/js/apifier.js
@@ -10,27 +10,99 @@ $.ajax({
    async: false,
    dataType: 'json',
    success: function (data) {
-       apifierData = data;
+       //apifierData = data;
        apifierApi = data.user;
    }
 });
 
 // VARIABLES
-var urlBase = 'https://api.apifier.com/v1/';
+var prefix = 'https://api.apifier.com/v1/';
 var api = apifierApi;
 var crawlId = '359hHayJT8Cefmu2R';
-var token = '?token=rQCrS5BnTTp3J46P7SkxHam89';
-var results = 'https://api.apifier.com/v1/execs/NmgfWdWMpgLye5KQ2/results';
+var suffix = '/crawlers/'+crawlId+'?token=rQCrS5BnTTp3J46P7SkxHam89';
+var urlSettings = prefix + apifierApi + suffix;
 
-// load recent results
+// Load apifier settings
+$.ajax({
+   url: urlSettings,
+   async: false,
+   dataType: 'json',
+   success: function (data) {
+     apifierSettings = data.lastExecution.resultsUrl;
+   }
+});
 
-// use the json from the above results
-// to load new data
-
-// loads test to data on page
-$(function(){
-  $.getJSON(results, function (data){
-    json = JSON.stringify(data);
-    console.log(json);
+// load data to page
+function displayAplifier(obj){
+  var temp = [];
+  var feedArray = [];
+  Object.keys(obj).forEach(function(key){
+    temp.push(obj[key].pageFunctionResult);
   });
+
+  // discard empty arrays
+  for(var a=0; a<temp.length; a++){
+    for(var b=0; b<5; b++){
+      if(temp[a][b].length > 0){
+        feedArray.push(temp[a][b]);
+      }
+    }
+  }
+
+  // create objects for easy deployment to page
+  // base, imageUrl, linkUrl, title, topic
+  var epicurious = feedArray[0];
+  var food52 = feedArray[1];
+  var luckypeach = feedArray[2];
+  var saveur = feedArray[3];
+  var foodandwine = feedArray[4];
+
+  // add label value to each arrays
+  epicurious.name = 'epicurious';
+  food52.name = 'food52';
+  luckypeach.name = 'luckypeach';
+  saveur.name = 'saveur';
+  foodandwine.name = 'foodandwine';
+
+
+
+  function show(obj){
+    // get object name
+    var objName = obj.name;
+    var link = '';
+
+    obj.forEach(function(dta){
+      // if links have http prefix...
+      if(dta.linkUrl.startsWith('http://')){
+        var tag = $('<a href="'+dta.linkUrl+'" target="_blank">'+dta.title+'</a><br>');
+        $("#"+objName).append(tag);
+      }
+      // if links DO NOT have http prefix...
+      else {
+        var link = dta.base + dta.linkUrl;
+
+        var tag1 = $('<a href="'+link+'" target="_blank">'+dta.title+'</a><br>');
+        $("#"+objName).append(tag1);
+      }
+    });
+  }
+
+  show(epicurious);
+  show(food52);
+  show(luckypeach);
+  show(saveur);
+  show(foodandwine);
+
+
+
+}
+
+// load APLIFIER data
+$.ajax({
+  url: apifierSettings,
+  async: false,
+  dataType: 'json',
+  success: function (data) {
+    displayAplifier(data);
+  }
 });
